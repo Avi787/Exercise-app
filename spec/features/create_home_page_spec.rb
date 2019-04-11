f046db86f274a11167e8c8ac07b68257c51c1f9a
Avi Tasks <workavi78@gmail.com>
2019-04-11 06:16:54 +0200

    Add bootstrap

--- spec/features/create_home_page_spec.rb
+++ spec/features/create_home_page_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Creating home page" do
     visit "/"
       
     expect(page).to have_content("Workout Lounge!")
-    expect(page).to have_content("show off your workout to your friends and family")
+    expect(page).to have_content("Show off your workout to your friends and family")
     expect(page).to have_link("Home")
     expect(page).to have_link("Athletes Den")
   end
